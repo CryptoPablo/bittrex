dd05160baa9d84cb4aceb15e2b2f8e80c1b5efff
Vladimir <vladimirkozlov657@gmail.com>
2019-05-11 00:39:46 +0300

    TwilioAdministrator class created

---
+++ src/main/java/com/vladimir/bittrexclient/config/twilio/TwilioAdministrator.java
@@ -0,0 +1,13 @@
+package com.vladimir.bittrexclient.config.twilio;
+
+public class TwilioAdministrator {
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
